a3813fb4417f6124e72054bdab77f26da6ad3c63
Mohit <mohitsharma8864@gmail.com>
2022-11-06 14:38:24 +0530

    Button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import * as React from 'react';
+import React,{useState} from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import './App.css';
 export default function App() {
+  const [active, setActive] = useState(false)
+  const handleSubmit =(e) => {
+    setActive(!active);
+  }
   return (
-    <Box
+    <div>
+      <Box
       component="form"
       sx={{
         '& > :not(style)': { m: 1, width: '25ch' },
@@ -14,5 +19,9 @@ export default function App() {
     >
       <TextField id="outlined-basic" label="Outlined" variant="outlined" />
     </Box>
+    <h1>
+      <button onClick={handleSubmit} className={active ? "red-btn" :"black-btn" }>Hello</button>
+    </h1>
+    </div>
   );
 }
